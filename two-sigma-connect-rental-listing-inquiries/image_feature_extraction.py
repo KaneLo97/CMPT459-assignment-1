10c32f425835c67823e4cde6b0f32b0695c49a7a
Varun Rustomji <varunrustomji@Varuns-MBP.hitronhub.home>
2020-02-05 22:17:51 -0800

    Image feature extraction

--- two-sigma-connect-rental-listing-inquiries/image_feature_extraction.py
+++ two-sigma-connect-rental-listing-inquiries/image_feature_extraction.py
@@ -6,10 +6,10 @@ import re
 import operator
 from zipfile import ZipFile
 from subprocess import check_output
-from PIL import Image
+from PIL import Image, ImageStat
 
 def exploreFiles():
-    
+
     root_dir = os.getcwd() #get the current directory
     folder = []
 
@@ -23,7 +23,7 @@ def exploreFiles():
                 #print (len(entries))
                 for entry in entries:
                     folder.append(di)
-    #print (folder)           
+    #print (folder)
     return folder
 
 def exploreImages():
@@ -38,39 +38,39 @@ def exploreImages():
             dir_basename = os.path.basename(dir_path)
             if(dir_basename.isnumeric()):
                 entries = os.listdir(dir_path)
-                for entry in entries: 
+                for entry in entries:
                     images_list.append(entry) #add all the images belonging to the listing_id to the images_list
                 if len(images_list) != 0:
                    complete_images_list.append((dir_basename,images_list)) #an array of tuples(folder name, folder images)
                 images_list = []
-               
+
     return complete_images_list
 
 
 def process_image(path):
 
     path = './images_sample/'+path[0:7]+'/'+path
-    img = np.array(Image.open(path))
-    #get dims
-    width = img.shape[1]
-    height = img.shape[0]
+    image = Image.open(path, 'r')
 
-    #flatten the image
-    img = img.transpose(2,0,1).reshape(3,-1)
+    width = image.width
+    height = image.height
 
-    #brightness is simple, assign 1 if zero to avoid divide
-    bright = np.amax(img,axis=0)
-    bright[bright==0] = 1
+    #mean brightness for the images
+    stat = ImageStat.Stat(image)
+    bright = stat.mean[0]
 
-    #hue, same, assign 1 if zero, not working atm due to arccos
-    denom = np.sqrt((img[0]-img[1])**2-(img[0]-img[2])*(img[1]-img[2]))
-    denom[denom==0] = 1
+    # image intensity
+    rgb_im = image.convert('RGB')
+    r, g, b = rgb_im.getpixel((1, 1))
+    intensity = (r+g+b) / 3
 
     #saturation
-    sat = (bright - np.amin(img,axis=0))/bright
+    max_color = max(r,g,b)
+    min_color = min(r,g,b)
+    sat = (max_color - min_color) / (max_color + min_color)
 
     #return mean values
-    return width,height,np.mean(bright),np.mean(sat)
+    return width,height,bright,np.mean(sat),intensity
 
     #print(img)
 
@@ -78,51 +78,35 @@ def process_row(row, image_files):
     result = []
     for image in image_files:
         if image[0] == str(row.listing_id):
-            # print (image[0])
-            # print (image)
             result = np.array([process_image(x) for x in image[1]])
+            
     result = np.mean(result,axis=0)
     row['image_width'] = result[0]
     row['image_height'] = result[1]
     row['image_brightness'] = result[2]
     row['image_saturation'] = result[3]
+    row['image_intensity'] = result[4]
+
     return row
-    
-'''  
-def process_row(row):
-    images = check_output(["ls", "./images_sample/"+str(row.listing_id)]).decode("utf8").strip().split('\n')
-    res = np.array([process_image(x) for x in images])
-    #print(images)
-    res = np.mean(res,axis=0)
-    row['image_width'] = res[0]
-    row['image_height'] = res[1]
-    row['image_brightness'] = res[2]
-    row['image_saturation'] = res[3]
-    return row
-'''
 
 
 def main():
-# 
-#     with ZipFile('images_sample.zip', 'r') as zipObj:
-#         # Extract all the contents of zip file in current directory
-#         zipObj.extractall()
-#         
-    
+#
+    # with ZipFile('images_sample.zip', 'r') as zipObj:
+        # Extract all the contents of zip file in current directory
+        # zipObj.extractall()
+#
+
     df = pd.read_json('train.json.zip')
     folder_files = exploreFiles()
     image_files = exploreImages()
     df = df[df.listing_id.isin(folder_files)]
     df = df.filter(['photos', 'listing_id'])
-    df['num_photos'] = df.apply(lambda x: len(x['photos']), axis = 1)
-    #print(df)
-    df = df[df['num_photos'] != 0]
-    
-
-    #df = df.apply(lambda row: process_row(row),axis=1)
+    df['num_images'] = df.apply(lambda x: len(x['photos']), axis = 1)
+    df = df[df['num_images'] != 0]
     df = df.apply(lambda row: process_row(row, image_files),axis=1)
     print (df)
-            
+
 
 if __name__ == "__main__":
     main()
